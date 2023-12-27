ecb8b51d7291562fbb7c6d7378f23cd22d5a84d8
HOBOOK <pkh879@gmail.com>
2023-12-27 17:30:55 +0900

    [ETC] gpt actions 테스트 코드

--- jobs/finder.job.js
+++ jobs/finder.job.js
@@ -46,19 +46,52 @@ const buildChatMessages = (now, issues) => {
             "content": [
                 "`You are a helpful assistant named ${name} who provides succinct answers in Markdown format.`"
             ]
-        },
+        }
     ]
 
     chatMessages.push({
         role: "user", 
         content: [
+            `${now} 시간의 구글 트랜드에 대해 검색해서 보여줘`,
            `${JSON.stringify(issues)}`
         ]
     })
 
+    console.log(chatMessages)
+
     return chatMessages
 }
 
-job()
+// const assistant = await openai.beta.assistants.create({
+//     name: "Math Tutor",
+//     instructions: "You are a personal math tutor. Write and run code to answer math questions.",
+//     tools: [{ type: "code_interpreter" }],
+//     model: "gpt-4-1106-preview"
+// });
 
+// const thread = await openai.beta.threads.create();
 
+// const message = await openai.beta.threads.messages.create(
+//     thread.id,
+//     {
+//       role: "user",
+//       content: "I need to solve the equation `3x + 11 = 14`. Can you help me?"
+//     }
+// );
+
+// const run = await openai.beta.threads.runs.create(
+//     thread.id,
+//     { 
+//       assistant_id: assistant.id,
+//       instructions: "Please address the user as Jane Doe. The user has a premium account."
+//     }
+// );
+
+// // const run = await openai.beta.threads.runs.retrieve(
+// //     thread.id,
+// //     run.id
+// // );
+
+// const messages = await openai.beta.threads.messages.list(
+//     thread.id
+// );
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,51 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+
+// SMTP 서버 설정
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com", // 메일 서비스 제공자의 SMTP 서버 주소
+  port: 587, // 통상적인 SMTP 포트
+  secure: false, // true면 465 포트 사용, false면 다른 포트 사용
+  auth: {
+    user: "vazilaboratory@gmail.com", // 이메일 전송을 위한 유저명 (이메일 주소)
+    pass: "lbpojbugmfthtgjl" // 이메일 전송을 위한 패스워드
+  }
+});
+
+
+router.post('/test', function(req, res, next) {
+  console.log('test')
+
+  const {to, status, data} = req.body
+
+  console.log(status)
+  console.log(to)
+  if(data && to && to.length > 0) {
+    for(let email of to) {
+      console.log('Send Start :: ' + email)
+      // 메일 옵션 설정
+      let mailOptions = {
+        from: '"바질 IT 트래커" <programming@vazilcompany.com>', // 발신자 정보
+        to: email, // 수신자 이메일 주소
+        subject: "[바질컴퍼니] ✔ 최신 IT 동향", // 제목
+        html: data, // 내용 (텍스트)
+      };
+
+      // 메일 전송
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(email, error);
+        }
+        console.log('Message sent: %s', email);
+      });
+    }
+  }
+  res.status(200).json('ok')
+})
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
